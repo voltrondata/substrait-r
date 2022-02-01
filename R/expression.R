4d1a1606e0e6e8919697d3c5d16c368aa74eb0ac
Dewey Dunnington <dewey@fishandwhistle.net>
2022-02-01 15:18:26 -0400

    implementation for double

--- R/expression.R
+++ R/expression.R
@@ -1,13 +1,36 @@
 
 #' @export
 as_substrait.double <- function(x, .ptype = NULL, ...) {
-  .qualified_name <- .qualified_name %||% "substrait.Expression.Literal.Decimal"
-  switch(
-    .qualified_name,
-    "substrait.Expression.Literal.Decimal" =
-      substrait$Expression$Literal$Decimal$create(x),
-    "substrait.Expression.Literal" =
-      substrait$Expression$Literal
-  )
-}
+  if (is.null(.ptype)) {
+    .ptype <- substrait$Expression$Literal$create(fp64 = NaN)
+  }
 
+  .qualified_name <- make_qualified_name(.ptype)
+  if (length(x) == 1 && !("list" %in% names(.ptype))) {
+    switch(
+      .qualified_name,
+      "substrait.Expression.Literal" = {
+        if (is.na(x) && !is.nan(x)) {
+          substrait$Expression$Literal$create(
+            null = substrait$Type$create(fp64 = list())
+          )
+        } else {
+          substrait$Expression$Literal$create(fp64 = x)
+        }
+      },
+      NextMethod()
+    )
+  } else {
+    switch(
+      .qualified_name,
+      "substrait.Expression.Literal" = {
+        substrait$Expression$Literal$create(
+          list = substrait$Expression$Literal$List$create(
+            lapply(x, as_substrait.double, .ptype = "substrait.Expression.Literal")
+          )
+        )
+      },
+      NextMethod()
+    )
+  }
+}
---
+++ tests/testthat/test-expression.R
@@ -0,0 +1,22 @@
+
+test_that("as_substrait() works for double()", {
+  expect_identical(
+    as_substrait(3.14),
+    substrait$Expression$Literal$create(fp64 = 3.14)
+  )
+
+  expect_identical(
+    as_substrait(c(3.14, 3.15), substrait$Expression$Literal$create(list = list())),
+    substrait$Expression$Literal$create(
+      list = substrait$Expression$Literal$List$create(
+        list(
+          as_substrait(3.14),
+          as_substrait(3.15)
+        )
+      )
+    )
+  )
+
+  expect_error(as_substrait(3.14, "substrait.NotAType"), "Can't create substrait")
+  expect_error(as_substrait(c(3.14, 3.15), "substrait.NotAType"), "Can't create substrait")
+})
