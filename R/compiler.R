2d61c2ddc11d30b5b5514be98ec7806e607be577
Dewey Dunnington <dewey@fishandwhistle.net>
2022-02-17 10:22:39 -0400

    add substrait_compiler_function() and document its arguments

--- R/compiler.R
+++ R/compiler.R
@@ -1,4 +1,33 @@
 
+#' Substrait compiler
+#'
+#' @param compiler a [substrait_compiler()]
+#' @param name A function name
+#' @param arg_types A `list()` of substrait.Type objects
+#' @param args A `list()` of arguments, which may be R objects or field
+#'   references and may be named (i.e., these have not yet been converted
+#'   into substrait.Expression objects).
+#' @param context An optional context that can be used to pick the correct
+#'   function (e.g., scalar, window, aggregate).
+#' @param pkg An R package name or NULL if no R package name was present
+#'   in the function call.
+#' @param ... Passed to S3 methods.
+#'
+#' @return
+#'   - `substrait_compiler()` returns an object of class "substrait_compiler"
+#'   - `substrait_compiler_function_id()` returns an integer identifier for
+#'     a function with a given name and argument types
+#'   - `substrait_compiler_function()` returns
+#'     a "substrait.Expression.ScalarFunction", a
+#'     "substrait.Expression.WindowFunction", or a "substrait.AggregateFunction"
+#'     depending on the `context`.
+#' @export
+#'
+#' @examples
+#' (compiler <- substrait_compiler())
+#' substrait_compiler_function_id(compiler, "some_func", list(integer()))
+#' substrait_compiler_function(compiler, "some_func", list(1L))
+#'
 substrait_compiler <- function() {
   compiler <- new.env(parent = emptyenv())
   # use 1 for starting IDs because 0 is the default protobuf value
@@ -17,9 +46,11 @@ substrait_compiler <- function() {
   )
 }
 
-substrait_compiler_function_id <- function(compiler, name, arg_types, pkg = NULL) {
-  arg_types <- lapply(arg_types, as_substrait, "substrait.Type")
-  key <- rlang::hash(list(pkg, name, arg_types))
+#' @rdname substrait_compiler
+#' @export
+substrait_compiler_function_id <- function(compiler, name, arg_types) {
+  arg_types <- unname(lapply(arg_types, as_substrait, "substrait.Type"))
+  key <- rlang::hash(list(name, arg_types))
 
   extension_function <- compiler$function_extensions[[key]]
   if (is.null(extension_function)) {
@@ -29,7 +60,7 @@ substrait_compiler_function_id <- function(compiler, name, arg_types, pkg = NULL
       ExtensionFunction$create(
         extension_uri_reference = compiler$extension_uri$extension_uri_anchor,
         function_anchor = substrait_compiler_next_id(compiler),
-        name = paste0(pkg %||% "_", "__", name)
+        name = name
       )
   }
 
@@ -40,4 +71,46 @@ substrait_compiler_next_id <- function(compiler) {
   id <- compiler$next_id
   compiler$next_id <- id + 1L
   id
+}
+
+#' @rdname substrait_compiler
+#' @export
+substrait_compiler_function <- function(compiler, name, args, pkg = NULL,
+                                        context = NULL, ...) {
+  UseMethod("substrait_compiler_function")
+}
+
+#' @export
+substrait_compiler_function.substrait_compiler <- function(compiler, name, args, pkg = NULL,
+                                                           context = NULL, ...) {
+  args <- lapply(args, as_substrait, "substrait.Expression")
+  prefix <- if (is.null(pkg)) "" else paste0(pkg, "__")
+  name <- paste0(prefix, name)
+  id <- substrait_compiler_function_id(compiler, name, args)
+  output_type <- substrait$Type$create()
+
+  type <- context$function_type %||% "scalar"
+  switch(
+    type,
+    "scalar" = substrait$Expression$ScalarFunction$create(
+      function_reference = id,
+      args = args
+    ),
+    "window" = substrait$Expression$WindowFunction$create(
+      function_reference = id,
+      args = args,
+      partitions = context$partitions %||% unspecified(),
+      sorts = context$sorts %||% unspecified(),
+      upper_bound = context$upper_bound %||% unspecified(),
+      lower_bound = context$lower_bound %||% unspecified(),
+      phase = context$phase %||% unspecified()
+    ),
+    "aggregate" = substrait$AggregateFunction$create(
+      function_reference = id,
+      args = args,
+      sorts = context$sorts %||% unspecified(),
+      phase = context$phase %||% unspecified()
+    ),
+    stop(sprintf("Unknown function type: '%s'", type))
+  )
 }
--- tests/testthat/test-compiler.R
+++ tests/testthat/test-compiler.R
@@ -8,23 +8,12 @@ test_that("substrait_compiler can be created", {
 test_that("substrait_compiler_function_id() works", {
   compiler <- substrait_compiler()
   expect_equal(
-    substrait_compiler_function_id(compiler, "some_fun", list(), pkg = "base"),
+    substrait_compiler_function_id(compiler, "some_fun", list()),
     1L
   )
   expect_equal(
-    substrait_compiler_function_id(compiler, "some_fun", list(), pkg = "base"),
+    substrait_compiler_function_id(compiler, "some_fun", list()),
     1L
-  )
-
-  # another package should trigger a new id
-  expect_equal(
-    substrait_compiler_function_id(
-      compiler,
-      "some_fun",
-      list(),
-      pkg = NULL
-    ),
-    2L
   )
 
   # different arg types should trigger a new id
@@ -34,10 +23,9 @@ test_that("substrait_compiler_function_id() works", {
       "some_fun",
       list(
         substrait$Type$create(i32 = list())
-      ),
-      pkg = NULL
+      )
     ),
-    3L
+    2L
   )
 
   # ...but doing it again should get the same id
@@ -47,10 +35,58 @@ test_that("substrait_compiler_function_id() works", {
       "some_fun",
       list(
         substrait$Type$create(i32 = list())
-      ),
-      pkg = NULL
+      )
     ),
-    3L
+    2L
+  )
+})
+
+test_that("substrait_compiler_function() works", {
+  compiler <- substrait_compiler()
+
+  expect_identical(
+    substrait_compiler_function(
+      compiler, "some_fun", list(1L),
+      context = list(function_type = "scalar")
+    ),
+    substrait$Expression$ScalarFunction$create(
+      function_reference = 1,
+      args = list(
+        substrait$Expression$create(
+          literal = substrait$Expression$Literal$create(i32 = 1L)
+        )
+      )
+    )
+  )
+
+  expect_identical(
+    substrait_compiler_function(
+      compiler, "some_fun", list(1L),
+      context = list(function_type = "window")
+    ),
+    substrait$Expression$WindowFunction$create(
+      function_reference = 1,
+      args = list(
+        substrait$Expression$create(
+          literal = substrait$Expression$Literal$create(i32 = 1L)
+        )
+      )
+    )
+  )
+
+  expect_identical(
+    substrait_compiler_function(
+      compiler, "some_fun", list(1L),
+      context = list(function_type = "aggregate")
+    ),
+    substrait$AggregateFunction$create(
+      function_reference = 1,
+      args = list(
+        substrait$Expression$create(
+          literal = substrait$Expression$Literal$create(i32 = 1L)
+        )
+      )
+    )
   )
 })
 
