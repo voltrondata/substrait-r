71b7484340e32df7899ee6a40e3fd1c99d04f1f2
Nic Crane <thisisnic@gmail.com>
2022-02-21 16:05:53 +0000

    Zero indexed tests

--- tests/testthat/test-dplyr-select.R
+++ tests/testthat/test-dplyr-select.R
@@ -2,11 +2,23 @@ library(dplyr)
 
 test_that("basic selection", {
   x <- base_table(mtcars)
-  out <- select(x, hp, mpg)
+  out <- select(x, cyl, carb)
 
   expect_named(out, "project")
   expect_length(out$project$expressions, 2)
-  expect_equal(out$project$expressions[[1]]$selection$direct_reference$struct_field$field, 4)
-  expect_equal(out$project$expressions[[2]]$selection$direct_reference$struct_field$field, 1)
+  expect_equal(out$project$expressions[[1]]$selection$direct_reference$struct_field$field, 1)
+  expect_equal(out$project$expressions[[2]]$selection$direct_reference$struct_field$field, 10)
 
 })
+
+# test_that("two selects equivalent to one", {
+#   x <- base_table(mtcars)
+#   out <- select(x, cyl, carb) %>%
+#     select(cyl)
+#
+#   expect_named(out, "project")
+#   expect_length(out$project$expressions, 2)
+#   expect_equal(out$project$expressions[[1]]$selection$direct_reference$struct_field$field, 1)
+#   expect_equal(out$project$expressions[[2]]$selection$direct_reference$struct_field$field, 10)
+#
+# })
