d65d34455ac16c8d87b75e158d2b10c3b4736959
Nic Crane <thisisnic@gmail.com>
2022-03-07 22:39:37 +0100

    Improve tests

--- tests/testthat/test-dplyr-filter.R
+++ tests/testthat/test-dplyr-filter.R
@@ -5,6 +5,9 @@ test_that("filter() can subset rows based on a precondition", {
     dplyr::filter(hp < 100)
 
   expect_s3_class(out, c("substrait_dplyr_query"))
+
+  expect_length(attributes(out)$filtered_rows, 1)
+
 })
 
 test_that("filter() can be used multiple times with multiple preconditions", {
@@ -15,4 +18,5 @@ test_that("filter() can be used multiple times with multiple preconditions", {
     dplyr::filter(am == 0)
 
   expect_s3_class(out, c("substrait_dplyr_query"))
+  expect_length(attributes(out)$filtered_rows, 2)
 })
