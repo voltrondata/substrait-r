8d88a048554260e27dd30d4c01e9e25ff08b1b2c
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-11 08:23:41 -0400

    tidy types.R

--- R/types.R
+++ R/types.R
@@ -1,98 +1,106 @@
+
 #' Substrait types
 #'
-#' @name substrait-type
-#' @return An Substrait type object
 #' @export
-substrait_boolean <- function(){
+#' @name substrait-type
+#' @return A substrait.Type proto object
+#'
+#' @examples
+#' substrait_boolean()
+#' substrait_i32()
+#' substrait_fp64()
+#' substrait_string()
+#'
+substrait_boolean <- function() {
   substrait$Type$create(bool_ = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_i8 <- function(){
+substrait_i8 <- function() {
    substrait$Type$create(i8 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_i16 <- function(){
+substrait_i16 <- function() {
    substrait$Type$create(i16 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_i32 <- function(){
+substrait_i32 <- function() {
    substrait$Type$create(i32 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_i64 <- function(){
+substrait_i64 <- function() {
    substrait$Type$create(i64 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_fp32 <- function(){
+substrait_fp32 <- function() {
    substrait$Type$create(fp32 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_fp64 <- function(){
+substrait_fp64 <- function() {
    substrait$Type$create(fp64 = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_string <- function(){
+substrait_string <- function() {
   substrait$Type$create(string = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_binary <- function(){
+substrait_binary <- function() {
   substrait$Type$create(binary = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_timestamp <- function(){
+substrait_timestamp <- function() {
   substrait$Type$create(timestamp = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_timestamp_tz <- function(){
+substrait_timestamp_tz <- function() {
   substrait$Type$create(timestamp_tz = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_date <- function(){
+substrait_date <- function() {
   substrait$Type$create(date = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_time <- function(){
+substrait_time <- function() {
   substrait$Type$create(time = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_interval_year <- function(){
+substrait_interval_year <- function() {
   substrait$Type$create(interval_year = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_interval_day <- function(){
+substrait_interval_day <- function() {
   substrait$Type$create(interval_day = list())
 }
 
 #' @rdname substrait-type
 #' @export
-substrait_uuid <- function(){
+substrait_uuid <- function() {
   substrait$Type$create(uuid = list())
 }
