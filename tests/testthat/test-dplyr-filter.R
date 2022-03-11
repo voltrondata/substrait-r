42a35734cfbf371598a23ce16d2b1538e9692feb
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-11 09:10:01 -0400

    tidy tests for filter()

--- tests/testthat/test-dplyr-filter.R
+++ tests/testthat/test-dplyr-filter.R
@@ -1,10 +1,16 @@
-test_that("filter() can subset rows based on a precondition", {
-  schema <- mtcars
 
-  out <- base_table(schema) %>%
+test_that("filter() with zero conditions is identical to its input", {
+  expect_identical(
+    base_table(mtcars),
+    dplyr::filter(base_table(mtcars))
+  )
+})
+
+test_that("filter() can subset rows based on an expression", {
+  out <- base_table(mtcars) %>%
     dplyr::filter(hp < 100)
 
-  expect_s3_class(out, c("substrait_dplyr_query"))
+  expect_s3_class(out, "substrait_dplyr_query")
 
   expect_identical(
     rlang::quo_text(attributes(out)$filtered_rows[[1]]),
@@ -12,17 +18,14 @@ test_that("filter() can subset rows based on a precondition", {
   )
 
   expect_length(attributes(out)$filtered_rows, 1)
-
 })
 
-test_that("filter() can be used multiple times with multiple preconditions", {
-  schema <- mtcars
-
-  out <- base_table(schema) %>%
+test_that("filter() can be used multiple times with multiple expressions", {
+  out <- base_table(mtcars) %>%
     dplyr::filter(hp < 100) %>%
     dplyr::filter(am == 0)
 
-  expect_s3_class(out, c("substrait_dplyr_query"))
+  expect_s3_class(out, "substrait_dplyr_query")
 
   expect_length(attributes(out)$filtered_rows, 2)
 
