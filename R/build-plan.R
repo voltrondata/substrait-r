a243e0efe4ab09b88989faefd04d81276d65699f
Nic Crane <thisisnic@gmail.com>
2022-03-14 12:23:54 +0100

    Initial commit

---
+++ R/build-plan.R
@@ -0,0 +1,47 @@
+#' @export
+build_plan <- function(x){
+  UseMethod("build_plan", x)
+}
+
+#' @export
+build_plan.substrait_dplyr_query <- function(x) {
+
+  data <- as.data.frame(x)
+  compiler <- substrait_compiler()
+
+  filtered_rows <- attr(x, "filtered_rows")
+
+  # do filtering before selection
+  if (!is_empty(filtered_rows)) {
+    plan <- substrait$FilterRel$create(
+      input = build_base_table(data),
+      condition = build_filters(data, filtered_rows, compiler)
+    )
+
+  }
+}
+
+build_base_table <- function(.data){
+
+  .data <- as.data.frame(.data)
+
+  # Currently using the known working example of reading in using a
+  # parquet file
+  temp_parquet_file <- tempfile()
+
+  #arrow::write_parquet(.data, temp_parquet_file)
+
+  input_table <- substrait$ReadRel$create(
+    base_schema = as_substrait(.data, "substrait.NamedStruct"),
+    local_files = substrait$ReadRel$LocalFiles$create(
+      items = list(
+        substrait$ReadRel$LocalFiles$FileOrFiles$create(
+          uri_file = sprintf("file://%s", temp_parquet_file),
+          format = substrait$ReadRel$LocalFiles$FileOrFiles$FileFormat$FILE_FORMAT_PARQUET
+        )
+      )
+    )
+  )
+
+  input_table
+}
