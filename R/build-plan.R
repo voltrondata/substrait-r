c4bc545bd4ccf7cb330e8024eb03f5d725c76292
Nic Crane <thisisnic@gmail.com>
2022-03-15 14:16:55 +0100

    Fix tests

--- R/build-plan.R
+++ R/build-plan.R
@@ -28,7 +28,7 @@ build_plan.substrait_dplyr_query <- function(x) {
 
   # Projection/Selection
   selected_columns <- attr(x, "selected_columns")
-  if (!rlang::is_empty(selected_columns) && selected_columns != names(x)) {
+  if (!rlang::is_empty(selected_columns) && !identical(names(selected_columns), names(x))) {
     plan <- substrait$ProjectRel$create(
       input = plan,
       expressions = build_projections(data, selected_columns)
@@ -49,5 +49,4 @@ build_base_table <- function(.data) {
   )
 
   substrait$Rel$create(read = input_table)
-
 }
