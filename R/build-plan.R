7db7f91f0cd68bbc090feef97a7bbc6e4b3fefea
Nic Crane <thisisnic@gmail.com>
2022-03-17 14:45:08 +0100

    Named table

--- R/build-plan.R
+++ R/build-plan.R
@@ -41,21 +41,10 @@ build_plan.substrait_dplyr_query <- function(x) {
 build_base_table <- function(.data) {
   .data <- as.data.frame(.data)
 
-  # Currently using the known working example of reading in using a
-  # parquet file
-  temp_parquet_file <- tempfile()
-
-  # arrow::write_parquet(.data, temp_parquet_file)
-
   input_table <- substrait$ReadRel$create(
     base_schema = as_substrait(.data, "substrait.NamedStruct"),
-    local_files = substrait$ReadRel$LocalFiles$create(
-      items = list(
-        substrait$ReadRel$LocalFiles$FileOrFiles$create(
-          uri_file = sprintf("file://%s", temp_parquet_file),
-          format = substrait$ReadRel$LocalFiles$FileOrFiles$FileFormat$FILE_FORMAT_PARQUET
-        )
-      )
+    named_table = substrait$ReadRel$NamedTable$create(
+      names = "the_name_of_the_table"
     )
   )
 
