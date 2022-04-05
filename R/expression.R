08ea40102cca5dff3781dcec836d8706f8fbb65a
Dewey Dunnington <dewey@fishandwhistle.net>
2022-04-05 10:13:52 -0300

    consolidate context and expression in expressions.R

--- R/expression.R
+++ R/expression.R
@@ -1,33 +1,6 @@
 
-# dd: As the name suggests, we need a `list()` of substrait.Expression objects
-# to use as a mask for evaluation (so that symbols map to an Expression, which
-# is probably a field reference for now). We also need a `list()` of
-# types so that we can convert the type of substrait.Expression that is a
-# field reference. I've wrapped both of these into `context` just to make
-# something work.
-new_context <- function(x = data.frame()) {
-  schema <- substrait_schema(x)
-
-  mask <- lapply(
-    # -1 because Substrait field references are 0-indexed
-    seq_along(schema$names) - 1,
-    simple_integer_field_reference
-  )
-  names(mask) <- schema$names
-
-  list(
-    schema = schema,
-    list_of_expressions = mask
-  )
-}
-
-# A note that both `context` and `list_of_expressions` needs to be hashed out
-# maybe the `list_of_expressions` is what our version of the "unbound table"
-# looks like?
-
 #' @export
-as_substrait.quosure <- function(x, .ptype = NULL, ..., compiler = SubstraitCompiler$new(),
-                                 context = NULL) {
+as_substrait.quosure <- function(x, .ptype = NULL, ..., compiler = SubstraitCompiler$new()) {
   if (is.null(.ptype)) {
     .ptype <- "substrait.Expression"
   }
@@ -36,15 +9,11 @@ as_substrait.quosure <- function(x, .ptype = NULL, ..., compiler = SubstraitComp
   switch(
     .qualified_name,
     "substrait.SortField" = {
-      mask <- context$list_of_expressions
-
       # evaluate the result using special rules for function calls
       result <- substrait_eval_expr(
         rlang::quo_get_expr(x),
         compiler = compiler,
-        context = context,
-        env = rlang::quo_get_env(x),
-        mask = mask
+        env = rlang::quo_get_env(x)
       )
 
       # ...but wrap result in SortField if it isn't already one
@@ -57,15 +26,11 @@ as_substrait.quosure <- function(x, .ptype = NULL, ..., compiler = SubstraitComp
       }
     },
     "substrait.Expression" = {
-      mask <- context$list_of_expressions
-
       # evaluate the result using special rules for function calls
       result <- substrait_eval_expr(
         rlang::quo_get_expr(x),
         compiler = compiler,
-        context = context,
-        env = rlang::quo_get_env(x),
-        mask = mask
+        env = rlang::quo_get_env(x)
       )
 
       # the result might be an atomic R object and not an Expression yet,
@@ -76,10 +41,10 @@ as_substrait.quosure <- function(x, .ptype = NULL, ..., compiler = SubstraitComp
   )
 }
 
-substrait_eval_expr <- function(x, compiler, context, env, mask) {
+substrait_eval_expr <- function(x, compiler, env) {
   if (rlang::is_call(x, c("$", "[["))) {
     if (rlang::is_symbol(x[[2]], ".data") && rlang::is_symbol(x[[1]], c("$", "[["))) {
-      return(rlang::eval_tidy(x, mask, env))
+      return(rlang::eval_tidy(x, compiler$mask, env))
     }
   }
 
@@ -97,7 +62,7 @@ substrait_eval_expr <- function(x, compiler, context, env, mask) {
     } else if (is.symbol(fun_expr)) {
       name <- as.character(fun_expr)
     } else {
-      return(rlang::eval_tidy(x, mask, env))
+      return(rlang::eval_tidy(x, compiler$mask, env))
     }
 
     # evaluate the arguments first (because we need the types to resolve
@@ -106,23 +71,21 @@ substrait_eval_expr <- function(x, compiler, context, env, mask) {
       x[-1],
       substrait_eval_expr,
       compiler = compiler,
-      context = context,
-      env = env,
-      mask = mask
+      env = env
     )
 
     # resolve the function call as an expression from the compiler
     # only scalar functions for now
     template <- substrait$Expression$ScalarFunction$create()
-    fun <- compiler$resolve_function(name, args, template, context = context)
+    fun <- compiler$resolve_function(name, args, template)
     substrait$Expression$create(scalar_function = fun)
   } else {
-    rlang::eval_tidy(x, mask, env)
+    rlang::eval_tidy(x, compiler$mask, env)
   }
 }
 
 #' @export
-as_substrait.substrait_Expression <- function(x, .ptype = NULL, ..., context = NULL) {
+as_substrait.substrait_Expression <- function(x, .ptype = NULL, ..., compiler = NULL) {
   if (is.null(.ptype)) {
     .ptype <- x
   }
@@ -140,20 +103,20 @@ as_substrait.substrait_Expression <- function(x, .ptype = NULL, ..., context = N
         "literal" = as_substrait(x[[which_expr_type]], .ptype),
         "selection" = {
           struct_field <- x$selection$direct_reference$struct_field
-          schema <- context$schema
+          schema <- compiler$schema
           if (is.null(schema)) {
-            stop("Can't guess field reference type without `context$schema`")
+            stop("Can't guess field reference type without `compiler$schema`")
           }
 
           # because 0 is the default value
           field <- struct_field$field %||% 0L
 
-          if (field < 0 || field >= length(context$schema$struct_$types)) {
+          if (field < 0 || field >= length(compiler$schema$struct_$types)) {
             stop(sprintf("Field reference out of bounds [%d]", field))
           }
 
           # because field is zero-indexed
-          context$schema$struct_$types[[field + 1]]
+          compiler$schema$struct_$types[[field + 1]]
         },
         "scalar_function" = x$scalar_function$output_type %||% substrait$Type$create(),
         "window_function" = x$window_function$output_type %||% substrait$Type$create(),
