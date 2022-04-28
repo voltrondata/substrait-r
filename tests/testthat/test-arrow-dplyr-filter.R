f5e39ad98793e28b560513b403b2c8edbac6c100
Nic Crane <thisisnic@gmail.com>
2022-04-28 22:52:30 +0100

    Skip if no arrow substrait

--- tests/testthat/test-arrow-dplyr-filter.R
+++ tests/testthat/test-arrow-dplyr-filter.R
@@ -1,6 +1,6 @@
 library(dplyr, warn.conflicts = FALSE)
 library(stringr)
-
+skip_if_not(has_arrow_with_substrait())
 
 test_that("filter() on is.na()", {
   skip("is.na() not implemented yet https://github.com/voltrondata/substrait-r/issues/72")
