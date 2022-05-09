2afbd12442d0b4a8b0f22bfac602351338cfc807
Dewey Dunnington <dewey@fishandwhistle.net>
2022-05-09 15:54:36 -0300

    Add some DuckDB function translations (#89)
    * add some translations and test them
    
    * fix CMD check NOTEs and WARNINGs

--- tests/testthat/test-pkg-duckdb.R
+++ tests/testthat/test-pkg-duckdb.R
@@ -10,6 +10,142 @@ test_that("duckdb_substrait_compiler() works", {
   expect_identical(as.data.frame(result), df)
 })
 
+test_that("duckdb translation for == and != works", {
+  skip_if_not(has_duckdb_with_substrait())
+  tbl <- tibble::tibble(col = c(1, 2, NA))
+
+  expect_identical(
+     tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(eq2 = col == 2, neq2 = col != 2) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      eq2 = c(FALSE, TRUE, NA),
+      neq2 = c(TRUE, FALSE, NA)
+    )
+  )
+})
+
+test_that("duckdb translation for & and |", {
+  skip_if_not(has_duckdb_with_substrait())
+  tbl <- tibble::tibble(col = c(TRUE, FALSE, NA))
+
+  expect_identical(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(
+        and_true = col & TRUE,
+        and_false = col & FALSE,
+        or_true = col | TRUE,
+        or_false = col | FALSE
+      ) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      and_true = c(TRUE, FALSE, NA),
+      and_false = c(FALSE, FALSE, FALSE),
+      or_true = c(TRUE, TRUE, TRUE),
+      or_false = c(TRUE, FALSE, NA)
+    )
+  )
+})
+
+test_that("duckdb translation for ! works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  tbl <- tibble::tibble(col = c(TRUE, FALSE))
+  expect_identical(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(
+        not = !col
+      ) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      not = c(FALSE, TRUE)
+    )
+  )
+})
+
+test_that("duckdb translation for ! handles NULL", {
+  skip_if_not(has_duckdb_with_substrait())
+  skip("duckdb translation for ! doesn't handle NULLs")
+
+  tbl <- tibble::tibble(col = c(TRUE, FALSE, NA))
+  expect_identical(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(
+        not = !col
+      ) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      not = c(FALSE, TRUE, NA)
+    )
+  )
+})
+
+test_that("duckdb translation for comparisons works", {
+  skip_if_not(has_duckdb_with_substrait())
+  tbl <- tibble::tibble(col = c(0, 1, 2, 3, NA))
+
+  expect_identical(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(
+        gt2 = col > 2,
+        gte2 = col >= 2,
+        lt2 = col < 2,
+        lte2 = col <= 2,
+        between_12 = dplyr::between(col, 1, 2)
+      ) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      gt2 = c(FALSE, FALSE, FALSE, TRUE, NA),
+      gte2 = c(FALSE, FALSE, TRUE, TRUE, NA),
+      lt2 = c(TRUE, TRUE, FALSE, FALSE, NA),
+      lte2 = c(TRUE, TRUE, TRUE, FALSE, NA),
+      between_12 = c(FALSE, TRUE, TRUE, FALSE, NA)
+    )
+  )
+})
+
+test_that("duckdb translation for arithmetic functions works", {
+  skip_if_not(has_duckdb_with_substrait())
+  tbl <- tibble::tibble(col = c(1, 2, NA))
+
+  expect_identical(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::transmute(
+        times2 = col * 2,
+        div2 = col / 2,
+        add2 = col + 2,
+        sub2 = col - 2,
+        pow2 = col ^ 2
+      ) %>%
+      dplyr::collect(),
+    tibble::tibble(
+      times2 = c(2, 4, NA),
+      div2 = c(1 / 2, 2 / 2, NA),
+      add2 = c(1 + 2, 2 + 2, NA),
+      sub2 = c(1 - 2, 2 - 2, NA),
+      pow2 = c(1 ^ 2, 2 ^ 2, NA)
+    )
+  )
+})
+
+test_that("duckdb translation for is.na() works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_identical(
+    tibble::tibble(col = c(1, 2, NA)) %>%
+      duckdb_substrait_compiler() %>%
+      dplyr::filter(is.na(col)) %>%
+      dplyr::collect(),
+    tibble::tibble(col = NA_real_)
+  )
+})
+
 test_that("duckdb can roundtrip a substrait plan", {
   skip_if_not(has_duckdb_with_substrait())
 
