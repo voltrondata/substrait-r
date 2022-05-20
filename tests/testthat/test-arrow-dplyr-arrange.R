0cab00417c54498e3ed31c7332cdc20c3ecf657a
Nicola Crane <thisisnic@gmail.com>
2022-05-20 16:58:59 +0100

    Enable arrange() tests to run on DuckDB engine (#119)

--- tests/testthat/test-arrow-dplyr-arrange.R
+++ tests/testthat/test-arrow-dplyr-arrange.R
@@ -1,31 +1,40 @@
 library(dplyr, warn.conflicts = FALSE)
 skip_if_not(has_arrow_with_substrait())
 
-skip("dplyr::arrange() doesn't currently work in Arrow via Substrait: https://github.com/voltrondata/substrait-r/issues/68")
 
 # randomize order of rows in test data
 tbl <- slice_sample(example_data, prop = 1L)
 
 test_that("arrange() on integer, double, and character columns", {
+
   compare_dplyr_binding(
+    # skip("dplyr::arrange() doesn't currently work in Arrow via Substrait: https://github.com/voltrondata/substrait-r/issues/68")
+    engine = "duckdb",
     .input %>%
       arrange(int, chr) %>%
       collect(),
     tbl
   )
   compare_dplyr_binding(
+    # skip("dplyr::arrange() doesn't currently work in Arrow via Substrait: https://github.com/voltrondata/substrait-r/issues/68")
+    engine = "duckdb",
     .input %>%
       arrange(int, desc(dbl)) %>%
       collect(),
     tbl
   )
+
+  skip("This fails on duckdb too - https://github.com/voltrondata/substrait-r/issues/122")
   compare_dplyr_binding(
+    # skip("dplyr::arrange() doesn't currently work in Arrow via Substrait: https://github.com/voltrondata/substrait-r/issues/68")
+    engine = "duckdb",
     .input %>%
       arrange(int, desc(desc(dbl))) %>%
       collect(),
     tbl
   )
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange(int) %>%
       arrange(desc(dbl)) %>%
@@ -33,12 +42,14 @@ test_that("arrange() on integer, double, and character columns", {
     tbl
   )
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange(int + dbl, chr) %>%
       collect(),
     tbl
   )
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       mutate(zzz = int + dbl, ) %>%
       arrange(zzz, chr) %>%
@@ -46,6 +57,7 @@ test_that("arrange() on integer, double, and character columns", {
     tbl
   )
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       mutate(zzz = int + dbl) %>%
       arrange(int + dbl, chr) %>%
@@ -53,19 +65,25 @@ test_that("arrange() on integer, double, and character columns", {
     tbl
   )
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       mutate(int + dbl) %>%
       arrange(int + dbl, chr) %>%
       collect(),
     tbl
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       group_by(lgl) %>%
       arrange(int, dbl) %>%
       collect(),
     tbl
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
     .input %>%
       group_by(lgl) %>%
@@ -73,6 +91,8 @@ test_that("arrange() on integer, double, and character columns", {
       collect(),
     tbl
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
     .input %>%
       group_by(lgl, grp2) %>%
@@ -81,6 +101,8 @@ test_that("arrange() on integer, double, and character columns", {
     tbl %>%
       mutate(grp2 = ifelse(is.na(lgl), 1L, as.integer(lgl)))
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
     .input %>%
       group_by(lgl) %>%
@@ -88,6 +110,8 @@ test_that("arrange() on integer, double, and character columns", {
       pull(lgl),
     tbl
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
     .input %>%
       arrange() %>%
@@ -95,6 +119,8 @@ test_that("arrange() on integer, double, and character columns", {
     tbl %>%
       group_by(lgl)
   )
+
+  skip("group_by not yet implemented: https://github.com/voltrondata/substrait-r/issues/28")
   compare_dplyr_binding(
     .input %>%
       group_by(lgl) %>%
@@ -102,7 +128,10 @@ test_that("arrange() on integer, double, and character columns", {
       collect(),
     tbl
   )
+
+  skip("error with empty arrange: https://github.com/voltrondata/substrait-r/issues/123")
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange() %>%
       collect(),
@@ -110,6 +139,7 @@ test_that("arrange() on integer, double, and character columns", {
   )
   test_sort_col <- "chr"
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange(!!sym(test_sort_col)) %>%
       collect(),
@@ -118,6 +148,7 @@ test_that("arrange() on integer, double, and character columns", {
   )
   test_sort_cols <- c("int", "dbl")
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange(!!!syms(test_sort_cols)) %>%
       collect(),
@@ -126,6 +157,8 @@ test_that("arrange() on integer, double, and character columns", {
 })
 
 test_that("arrange() on datetime columns", {
+
+  skip("datetime types not yet supported: https://github.com/voltrondata/substrait-r/issues/124")
   compare_dplyr_binding(
     .input %>%
       arrange(dttm, int) %>%
@@ -143,6 +176,7 @@ test_that("arrange() on datetime columns", {
 
 test_that("arrange() on logical columns", {
   compare_dplyr_binding(
+    engine = "duckdb",
     .input %>%
       arrange(lgl, int) %>%
       collect(),
@@ -151,6 +185,47 @@ test_that("arrange() on logical columns", {
 })
 
 test_that("arrange() with bad inputs", {
+
+  # test on DuckDB here
+  compare_dplyr_binding(
+    engine = "duckdb",
+    .input %>%
+      arrange(1) %>%
+      collect(),
+    example_data
+  )
+  compare_dplyr_binding(
+    engine = "duckdb",
+    .input %>%
+      arrange(2 + 2) %>%
+      collect(),
+    example_data
+  )
+
+  expect_error(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      arrange(aertidjfgjksertyj),
+    "not found",
+    fixed = TRUE
+  )
+  expect_error(
+    tbl %>%
+      duckdb_substrait_compiler() %>%
+      arrange(desc(aertidjfgjksertyj + iaermxiwerksxsdqq)),
+    "not found",
+    fixed = TRUE
+  )
+
+  skip("desc not yet properly implemented: https://github.com/voltrondata/substrait-r/issues/125")
+  compare_dplyr_error(
+    .input %>%
+      arrange(desc(int, chr)) %>%
+      collect(),
+    example_data
+  )
+
+  skip("dplyr::arrange() doesn't currently work in Arrow via Substrait: https://github.com/voltrondata/substrait-r/issues/68")
   expect_error(
     tbl %>%
       arrow_substrait_compiler() %>%
@@ -179,11 +254,5 @@ test_that("arrange() with bad inputs", {
     "not found",
     fixed = TRUE
   )
-  expect_error(
-    tbl %>%
-      arrow_substrait_compiler() %>%
-      arrange(desc(int, chr)),
-    "expects only one argument",
-    fixed = TRUE
-  )
+
 })
