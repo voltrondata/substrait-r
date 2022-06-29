02d2d67ce172261097da9384e2b8348444ac3796
Nic Crane <thisisnic@gmail.com>
2022-06-29 09:00:21 +0100

    Update tests/testthat/test-pkg-arrow.R

--- tests/testthat/test-pkg-arrow.R
+++ tests/testthat/test-pkg-arrow.R
@@ -37,7 +37,7 @@ test_that("ArrowSubstraitCompiler can translate simple unary and binary calls", 
 
   expect_error(
     compiler$resolve_function("not_a_fun!", list(), list()),
-    'could not find function "not_a_fun!"'
+    'Could not find function "not_a_fun!"'
   )
 
   expect_error(
