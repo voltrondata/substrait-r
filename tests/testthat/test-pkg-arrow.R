ad2a5ec34c73b73fd8972ffa94ef7daf98f9a0f1
Nic Crane <thisisnic@gmail.com>
2023-01-20 16:06:25 +0000

    Move tests to package specific files

--- tests/testthat/test-pkg-arrow.R
+++ tests/testthat/test-pkg-arrow.R
@@ -297,3 +297,75 @@ test_that("substrait_eval_arrow() can evaluate a plan with one read relation", {
     error = TRUE
   )
 })
+
+test_that("arrow translation for != works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl != 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9, 9))
+  )
+})
+
+test_that("arrow translation for == works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl == 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = 0)
+  )
+})
+
+test_that("arrow translation for < works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl < 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9))
+  )
+})
+
+test_that("arrow translation for > works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl > 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = 9)
+  )
+})
+
+test_that("arrow translation for <= works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl <= 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9, 0))
+  )
+})
+
+test_that("arrow translation for >= works", {
+  skip_if_not(has_arrow_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      arrow_substrait_compiler() %>%
+      substrait_filter(dbl >= 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(0, 9))
+  )
+})
--- tests/testthat/test-pkg-duckdb.R
+++ tests/testthat/test-pkg-duckdb.R
@@ -236,3 +236,76 @@ test_that("duckdb raises error for empty projection", {
     "Column list must not be empty"
   )
 })
+
+
+test_that("duckdb translation for != works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl != 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9, 9))
+  )
+})
+
+test_that("duckdb translation for == works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl == 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = 0)
+  )
+})
+
+test_that("duckdb translation for < works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl < 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9))
+  )
+})
+
+test_that("duckdb translation for > works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl > 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = 9)
+  )
+})
+
+test_that("duckdb translation for <= works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl <= 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(-999, -99, -9, 0))
+  )
+})
+
+test_that("duckdb translation for >= works", {
+  skip_if_not(has_duckdb_with_substrait())
+
+  expect_equal(
+    example_data[1:5, "dbl"] %>%
+      duckdb_substrait_compiler() %>%
+      substrait_filter(dbl >= 0) %>%
+      dplyr::collect(),
+    tibble::tibble(dbl = c(0, 9))
+  )
+})
