cdf6ee414a7e4ef347c1f916107fee9109006b2f
Dewey Dunnington <dewey@voltrondata.com>
2023-02-22 11:31:56 -0400

    basic test of join

--- R/join-rel.R
+++ R/join-rel.R
@@ -1,5 +1,5 @@
 
-substrait_join <- function(compiler_left, compiler_right, by,
+substrait_join <- function(compiler_left, compiler_right, by = NULL,
                            type = "JOIN_TYPE_INNER",
                            name_repair = join_name_repair_none,
                            emit = join_emit_all) {
@@ -9,19 +9,21 @@ substrait_join <- function(compiler_left, compiler_right, by,
   if (inherits(compiler_left, "SubstraitCompiler") &&
     !inherits(compiler_right, "SubstraitCompiler")) {
     compiler <- compiler_left$clone()
-    right_read_rel <- compiler$add_named_table(compiler)
+    right_read_rel <- compiler$add_named_table(compiler_right)
 
     left_rel <- compiler$rel
     left_schema <- compiler$schema
 
     right_rel <- substrait$Rel$create(read = right_read_rel)
-    right_schema <- right_read_rel$schema
+    right_schema <- right_read_rel$base_schema
   } else if (!inherits(compiler_left, "SubstraitCompiler") &&
     inherits(compiler_right, "SubstraitCompiler")) {
     compiler <- compiler_right$clone()
-    left_read_rel <- compiler$add_named_table(compiler)
-    left_rel <- substrait$Rel$create(read = right_read_rel)
-    left_schema <- right_read_rel$schema
+    left_read_rel <- compiler$add_named_table(compiler_left)
+
+    left_rel <- substrait$Rel$create(read = left_read_rel)
+    left_schema <- left_read_rel$base_schema
+
     right_rel <- compiler$rel
     right_schema <- compiler$schema
   } else if (!inherits(compiler_left, "SubstraitCompiler") &&
@@ -30,30 +32,32 @@ substrait_join <- function(compiler_left, compiler_right, by,
       substrait_join(
         substrait_compiler(compiler_left),
         compiler_right,
-        expression = expression,
-        type = type
+        by = by,
+        type = type,
+        name_repair = name_repair,
+        emit = emit
       )
     )
   } else {
     # This is probably hard...we need to walk the extensions and give them
     # new IDs, then walk the relations and all expressions they contain
-    #
-    abort("Merging substrait compilers is not yet implemented")
+    # to replace the functions with new IDs.
+    rlang::abort("Merging substrait compilers is not yet implemented")
   }
 
   # Update the compiler schema and mask before evaluating the join expression
   compiler$schema <- substrait$NamedStruct$create(
     names = name_repair(by, left_schema$names, right_schema$names),
-    struct_ = substrait$Type$Struct$create(
-      types = c(left_schema$struct_$types, right_schema$struct_)
+    struct = substrait$Type$Struct$create(
+      types = c(left_schema$struct$types, right_schema$struct$types)
     )
   )
 
   compiler$.data <- lapply(
-    seq_along(.compiler$schema$struct_$types) - 1L,
+    seq_along(compiler$schema$struct$types) - 1L,
     simple_integer_field_reference
   )
-  names(.compiler$.data) <- .compiler$schema$names
+  names(compiler$.data) <- compiler$schema$names
 
   # The `by` expression resolves field references differently and injects them
   # by value; however, the compiler is needed because the == and & functions
@@ -66,7 +70,9 @@ substrait_join <- function(compiler_left, compiler_right, by,
   rel <- substrait$Rel$create(
     join = substrait$JoinRel$create(
       common = substrait$RelCommon$create(
-        emit = emit(by, left_schema$names, right_schema$names)
+        emit = substrait$RelCommon$Emit$create(
+          output_mapping = emit(by, left_schema$names, right_schema$names)
+        )
       ),
       left = left_rel,
       right = right_rel,
@@ -81,7 +87,10 @@ substrait_join <- function(compiler_left, compiler_right, by,
 }
 
 as_join_expression <- function(by, names_left, names_right) {
-  if (rlang::is_named(by)) {
+  if (is.null(by)) {
+    by_left <- intersect(names_left, names_right)
+    by_right <- by_left
+  } else if (rlang::is_named(by)) {
     by_left <- names(by)
     by_right <- unname(by)
   } else {
@@ -95,7 +104,9 @@ as_join_expression <- function(by, names_left, names_right) {
   } else if (is.integer(by_right)) {
     by_left <- as.integer(by_left)
   } else {
-    abort("Only join expressions in the form c('name_left' = 'name_right') are supported")
+    rlang::abort(
+      "Only join expressions in the form c('name_left' = 'name_right') are supported"
+    )
   }
 
   by_left <- lapply(by_left, simple_integer_field_reference)
--- tests/testthat/test-join-rel.R
+++ tests/testthat/test-join-rel.R
@@ -1,4 +1,34 @@
 
-test_that("substrait_join() adds data sources", {
-  expect_equal(2 * 2, 4)
+test_that("substrait_join() combines data sources correctly", {
+  # Requires that a translation exists for '==' to generate the join
+  # expression, so using duckdb for now
+  skip_if_not(has_duckdb_with_substrait())
+
+  df_left <- data.frame(number = 1:26, letter = letters, stringsAsFactors = FALSE)
+  df_right <- data.frame(number = 1:26, LETTER = LETTERS, stringsAsFactors = FALSE)
+
+  join_compiler_df <- substrait_join(duckdb_substrait_compiler(df_left), df_right)
+  expect_identical(
+    join_compiler_df$rel$join$left$read$base_schema$names,
+    names(df_left)
+  )
+  expect_identical(
+    join_compiler_df$rel$join$right$read$base_schema$names,
+    names(df_right)
+  )
+
+  join_df_compiler <- substrait_join(df_left, duckdb_substrait_compiler(df_right))
+  expect_identical(
+    join_compiler_df$rel$join$left$read$base_schema$names,
+    names(df_left)
+  )
+  expect_identical(
+    join_compiler_df$rel$join$right$read$base_schema$names,
+    names(df_right)
+  )
+
+  expect_error(
+    substrait_join(substrait_compiler(df_left), substrait_compiler(df_right)),
+    "Merging substrait compilers is not yet implemented"
+  )
 })
