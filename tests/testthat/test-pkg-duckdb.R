ca183545f4c9fc815bede3b1fc32a9ec11ca9341
Nic Crane <thisisnic@gmail.com>
2023-02-27 14:40:18 +0000

    Swap tests

--- tests/testthat/test-pkg-duckdb.R
+++ tests/testthat/test-pkg-duckdb.R
@@ -376,7 +376,7 @@ test_that("duckdb translation for round works", {
       substrait_project(x, y = round(x)) %>%
       dplyr::collect(),
     tibble::tibble(
-      y = c(1, 2, 3, 4)
+      y = c(1, 2, 3, 5)
     )
   )
 
@@ -398,7 +398,7 @@ test_that("duckdb translation for round works", {
       dplyr::collect(),
     tibble::tibble(
       x = c(1, 2.34, 3.456, 4.5),
-      y = c(1, 2, 3, 5)
+      y = c(1, 2, 3, 4)
     )
   )
 
