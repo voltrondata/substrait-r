c5de18173d58ebb39732c60a8dd33d2a87e5fb3c
Nic Crane <thisisnic@gmail.com>
2023-03-14 11:49:42 +0000

    Build docs and fix typo

--- tests/testthat/test-pkg-dplyr.R
+++ tests/testthat/test-pkg-dplyr.R
@@ -366,7 +366,7 @@ test_that("count.SubstraitCompiler works", {
   skip_if_not_installed("dplyr")
 
   compiler <- substrait_compiler(data.frame(a = c(1, 1, 2, 2, 3)))
-  out <- distinct(compiler)
+  out <- count(compiler)
   plan <- out$plan()
   expect_s3_class(plan$relations[[1]]$root$input$aggregate, "substrait_AggregateRel")
   #TODO: work out what to test here
